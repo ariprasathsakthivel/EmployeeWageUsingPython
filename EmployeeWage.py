e5baefeeb4cf1858d202c5e4530da74b457620b8
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-09-08 21:59:13 +0530

    [Ariprasath] Add: calculating the total employee wage for a month. assumed 20 working days per month.

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -2,24 +2,22 @@
 @Author: Ariprasath
 @Date: 2021-09-08 20:51:00
 @Last Modified by: Ariprasath
-@Last Modified time: 2021-09-08 20:00:00
+@Last Modified time: 2021-09-08
 @Title : Check employee is present or absent
 '''
 
 
+from functools import total_ordering
 import random
 
 wage_per_hour=20
-num=random.randint(0,2)
-full_day_wage=8*wage_per_hour
-half_day_work=4*wage_per_hour
-switcher={
-    0:
-    "Employee is present; Total employee wage is 160",
-    1:
-    "Employee is present for half day; Total employee wage is 80",
-    2:
-    "Employee is absent; Total employee wage is 0"
-}
-
-print(switcher.get(num))
+day=0
+total_emp_wage=0
+while day<20:
+    num=random.randint(0,2)
+    if num==0:
+        total_emp_wage=total_emp_wage+8*wage_per_hour
+    elif num==1:
+        total_emp_wage=total_emp_wage+4*wage_per_hour
+    day+=1
+print(f"Total employee wage is {total_emp_wage}")
