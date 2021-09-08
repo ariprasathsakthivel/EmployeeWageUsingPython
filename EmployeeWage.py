753e5037618d1bd437641853649eb11a6a1fc7b7
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-09-08 21:12:46 +0530

    [Ariprasath] Add: checking whether the Employee is present or absent by using a random number generated by random module

---
+++ EmployeeWage.py
@@ -0,0 +1,16 @@
+'''
+@Author: Ariprasath
+@Date: 2021-09-08 20:51:00
+@Last Modified by: Ariprasath
+@Last Modified time: 2021-09-08 20:00:00
+@Title : Check employee is present or absent
+'''
+
+
+import random
+
+num=random.randint(0,1)
+if num==0:
+    print("Employee is present")
+else:
+    print("Employee is absent")
