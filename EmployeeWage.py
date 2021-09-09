7928b94f667c63c1db3cce7d43b07ed0dc741043
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-09-09 07:55:22 +0530

    [Ariprasath] Refactor: calculation of total working hours is retrevied from a function

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -2,24 +2,34 @@
 @Author: Ariprasath
 @Date: 2021-09-08 20:51:00
 @Last Modified by: Ariprasath
-@Last Modified time: 2021-09-08
+@Last Modified time: 2021-09-09
 @Title : calculating wages for 20 working days or 100 hours
 '''
 
 
 import random
 
-wage_per_hour=20
-day=0
-total_emp_wage=0
-working_hours=0
-while day<20 and working_hours<=100:
-    num=random.randint(0,2)
-    if num==0:
-        total_emp_wage=total_emp_wage+8*wage_per_hour
-        working_hours=working_hours+8
-    elif num==1:
-        total_emp_wage=total_emp_wage+4*wage_per_hour
-        working_hours=working_hours+4
-    day+=1
-print(f"Total employee wage is {total_emp_wage}")
+def emp_hours():
+    '''
+    Description:
+        calculates the total employee working hours untill an employee working days of the month reaches 20 or total working hours of the month reaches 100 hours.
+    parameter:
+        None
+    Return:
+        working_hours(int): Total working hours in a month
+    '''
+    day=0
+    working_hours=0
+    while day<20 and working_hours<=100:
+        num=random.randint(0,2)
+        if num==0:
+            working_hours=working_hours+8
+        elif num==1:
+            working_hours=working_hours+4
+        day+=1
+    return working_hours
+
+if __name__=="__main__":
+    wage_per_hour=20
+    total_emp_wage=emp_hours()*wage_per_hour
+    print(f"Total employee wage is {total_emp_wage}")
